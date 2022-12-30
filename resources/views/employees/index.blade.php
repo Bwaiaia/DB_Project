41605a13a2b00658ec6f41df468a28071278fdbf
Pickorea <38229544+Pickorea@users.noreply.github.com>
2022-12-30 15:28:11 +1200

    composer update

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,14 +1,25 @@
-<html>
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<ul class="list-group">
-    @foreach($employees as $employee)
-  <li class="list-group-item">{{$employee->name}}</li>
-  <li class="list-group-item">{{$employee->age}}</li>
-  <li class="list-group-item">{{$employee->island}}</li>
-  @endforeach
-</ul>
+                <div class="card-body">
 
-<!-- <ul class="list-group"> -->
-
-</html>
+              <ul class="list-group">
+                  @foreach($employees as $employee)
+                <li class="list-group-item">{{$employee->name}}</li>
+                <li class="list-group-item">{{$employee->age}}</li>
+                <li class="list-group-item">{{$employee->island}}</li>
+                @endforeach
+              </ul>
+            
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
