66f4766da17328a59ad4053d77ed2b9d825cabc5
Bwaiaia <neii.meeraatiimi@gmail.com>
2023-01-04 14:26:36 +1200

    village

---
+++ app/Http/Controllers/VillageController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use App\Models\Island;
+use App\Models\Village;
+
+class VillageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        $villages = Village::all();
+        //dd($employees);
+
+        // Pass data to view
+        return view('village.index', ['village' => $villages]);
+
+        //return 'welcome'; //view('employees.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('villages.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+            $input = $request->all();
+        
+            $results = Village::create($input);
+
+
+        return redirect()->route('village.index')->with('exception', 'Operation failed !');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $village = Village::find($id);
+
+		return view('villages.show')
+	        ->with('village',$village);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $village = Village::find($id);
+		return view('villages.edit')->withVillage($village);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // $island = $request->all();
+     
+        $data = Village::find($id)->update($request->all());
+
+
+           return redirect()->route('village.index')->with('message', 'Updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employee $employee)
+    {
+        //
+    }
+}
---
+++ app/Models/Village.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Console\Helper\Table;
+
+class Village extends Model
+{
+    use HasFactory;
+
+    protected $table = 'villages';
+    protected $fillable = ['name', 'description'];
+
+}
