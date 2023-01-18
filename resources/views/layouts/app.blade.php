5f7eda98014728daf6ec62596f442f28bb7be25e
Pickorea <38229544+Pickorea@users.noreply.github.com>
2023-01-18 13:06:14 +1200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
 
   <meta name="copyright" content="MACode ID, https://macodeid.com/">
 
-  <title>Revetive - Free Business Corporate Template By MACode ID</title>
-
   <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
   
   <link rel="stylesheet" href="{{asset('assets/css/maicons.css')}}">
